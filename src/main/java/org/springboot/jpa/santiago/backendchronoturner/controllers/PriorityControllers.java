eb3a068002517f1e9a9e07d4309ecaa63a769ffa
Santiago <santimh04@gmail.com>
2024-09-19 12:38:59 -0500

    feature: PriorityControllers file created. Pending for implementation and error handling

---
+++ src/main/java/org/springboot/jpa/santiago/backendchronoturner/controllers/PriorityControllers.java
@@ -0,0 +1,16 @@
+package org.springboot.jpa.santiago.backendchronoturner.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/priority")
+@Tag(name = "Priorities")
+public class PriorityControllers {
+    //Atributos de PriorityControllers
+    //Constructores de PriorityControllers
+    //Asignadores de atributos de PriorityControllers (setters)
+    //Lectores de atributos de PriorityControllers (getters)
+    //Métodos de PriorityControllers
+}
