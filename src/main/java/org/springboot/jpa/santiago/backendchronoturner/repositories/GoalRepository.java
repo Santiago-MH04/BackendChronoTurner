d0857df2b77211f7decb108c68b3b1351853977c
Santiago <santimh04@gmail.com>
2024-09-18 20:51:53 -0500

    feature: repository interfaces created. They extend JpaRepository<Entity, ID>

---
+++ src/main/java/org/springboot/jpa/santiago/backendchronoturner/repositories/GoalRepository.java
@@ -0,0 +1,12 @@
+package org.springboot.jpa.santiago.backendchronoturner.repositories;
+
+import org.springboot.jpa.santiago.backendchronoturner.entities.Goal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoalRepository extends JpaRepository<Goal, String> {
+    //Atributos de GoalRepository
+    //Constructores de GoalRepository
+    //Asignadores de atributos de GoalRepository (setters)
+    //Lectores de atributos de GoalRepository (getters)
+    //Métodos de GoalRepository
+}
---
+++ src/main/java/org/springboot/jpa/santiago/backendchronoturner/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package org.springboot.jpa.santiago.backendchronoturner.repositories;
+
+import org.springboot.jpa.santiago.backendchronoturner.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    //Atributos de UserRepository
+    //Constructores de UserRepository
+    //Asignadores de atributos de UserRepository (setters)
+    //Lectores de atributos de UserRepository (getters)
+    //Métodos de UserRepository
+}
