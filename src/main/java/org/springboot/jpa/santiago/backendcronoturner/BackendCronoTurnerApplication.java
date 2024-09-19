1896aadaa0702601edc31fff68ac5b3cf06bbdd5
Santiago <santimh04@gmail.com>
2024-09-18 19:32:45 -0500

    feature: first steps into rebuilding

---
+++ src/main/java/org/springboot/jpa/santiago/backendcronoturner/BackendCronoTurnerApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.jpa.santiago.backendcronoturner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCronoTurnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCronoTurnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springboot/jpa/santiago/backendcronoturner/BackendCronoTurnerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.jpa.santiago.backendcronoturner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCronoTurnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
