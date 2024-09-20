3859259d6262e4d93f7d2c40ed1254d878a1d43f
Santiago <santimh04@gmail.com>
2024-09-20 11:04:56 -0500

    feature: UserController error handling implementation

--- src/main/java/org/springboot/jpa/santiago/backendchronoturner/dto/Error.java
+++ src/main/java/org/springboot/jpa/santiago/backendchronoturner/dto/Error.java
@@ -1,9 +1,51 @@
 package org.springboot.jpa.santiago.backendchronoturner.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/*@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder*/
 public class Error {    //Esta es una clase DTO con el mensaje personalizado de error
-    //Atributos de CLASE
+        //Atributos de CLASE
+    private String errorName;
+    private String message;
+    private int status;
+    private LocalDateTime date; //Este es el que genera el timestamp
+
     //Constructores de CLASE
-    //Asignadores de atributos de CLASE (setters)
-    //Lectores de atributos de CLASE (getters)
+        //Asignadores de atributos de CLASE (setters)
+    public void setErrorName(String errorName) {
+        this.errorName = errorName;
+    }
+        public void setMessage(String message) {
+            this.message = message;
+        }
+            public void setStatus(int status) {
+                this.status = status;
+            }
+                public void setDate(LocalDateTime date) {
+                    this.date = date;
+                }
+
+        //Lectores de atributos de CLASE (getters)
+    public String getErrorName() {
+        return this.errorName;
+    }
+        public String getMessage() {
+            return this.message;
+        }
+            public int getStatus() {
+                return this.status;
+            }
+                public LocalDateTime getDate() {
+                    return this.date;
+                }
+
     //Métodos de CLASE
 }
